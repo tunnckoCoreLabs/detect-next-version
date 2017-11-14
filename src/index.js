2c7fb20c8dd2580dcceaad3dbe328ea52fcb1c17
Charlike Mike Reagent <olsten.larck@gmail.com>
2017-11-14 07:53:57 +0200

    fix(misc): update, lint, docs
    Signed-off-by: Charlike Mike Reagent <olsten.larck@gmail.com>

--- src/index.js
+++ src/index.js
@@ -3,36 +3,76 @@
  * @license Apache-2.0
  */
 
-const parseCommitMessage = require('parse-commit-message');
+const parseCommitMessage = require('parse-commit-message')
 
-module.exports = function detectChange (commitMessage, full) {
-  const result = parseCommitMessage(commitMessage, incrementMapper);
-  return full === true ? result : result.increment;
-};
+/**
+ * > Parses given `commitMessage` and returns an **"increment" type**,
+ * like `'patch'`, `'minor'` or `'major'`. That's useful to be passed
+ * to the [semver][]'s `inc()` method (see below example).
+ *
+ * > If you pass `full` param, then a **"commit" object** will be return,
+ * which comes directly from [parse-commit-message][] and contains few properties
+ * like `type`, `scope`, `subject`, `increment`, `body`, `isBreaking` and etc.
+ *
+ * **Example**
+ *
+ * ```js
+ * const semver = require('semver')
+ * const detectNext = require('detect-next-version')
+ *
+ * const commitMessage = 'fix(ng-list): updates the list order, thanks @hercules'
+ *
+ * const increment = detectNext(commitMessage)
+ * console.log(increment) // => minor
+ *
+ * const commit = detectNext(commitMessage, true)
+ * // => { type: 'fix',
+ * // scope: 'ng-list',
+ * // subject: 'updates the list order, thanks @hercules',
+ * // header: 'fix(ng-list): updates the list order, thanks @hercules',
+ * // body: null,
+ * // footer: null,
+ * // increment: 'patch',
+ * // isBreaking: false }
+ *
+ * const nextVersion = semver.inc('1.1.0', commit.increment)
+ * console.log(nextVersion) // => 1.1.1
+ * ```
+ *
+ * @param  {string}  `commitMessage` a single commit message, including the new lines
+ * @param  {boolean} `full` pass `true` if you want to get commit object, instead of increment type
+ * @return {string|Object} if string it is **"increment" type**, otherwise **"commit" object**
+ * @api public
+ */
+
+module.exports = function detectNextVersion (commitMessage, full) {
+  const result = parseCommitMessage(commitMessage, incrementMapper)
+  return full === true ? result : result.increment
+}
 
 function incrementMapper (commit) {
-  const isBreaking = isBreakingChange(commit);
-  let increment = null;
+  const isBreaking = isBreakingChange(commit)
+  let increment = null
 
   if (/fix|bugfix|patch/.test(commit.type)) {
-    increment = 'patch';
+    increment = 'patch'
   }
   if (/feat|feature|minor/.test(commit.type)) {
-    increment = 'minor';
+    increment = 'minor'
   }
   if (/break|breaking|major/.test(commit.type) || isBreaking) {
-    increment = 'major';
+    increment = 'major'
   }
 
-  return Object.assign({}, commit, { increment, isBreaking });
+  return Object.assign({}, commit, { increment, isBreaking })
 }
 
 /* eslint-disable no-param-reassign */
 
 function isBreakingChange ({ subject, body, footer }) {
-  body = body || '';
-  footer = footer || '';
+  body = body || ''
+  footer = footer || ''
 
-  const re = 'BREAKING CHANGE:';
-  return subject.includes(re) || body.includes(re) || footer.includes(re);
+  const re = 'BREAKING CHANGE:'
+  return subject.includes(re) || body.includes(re) || footer.includes(re)
 }
--- test/index.js
+++ test/index.js
@@ -3,61 +3,61 @@
  * @license Apache-2.0
  */
 
-const test = require('mukla');
-const isObject = require('isobject');
-const detectNextVersion = require('../src/index.js');
+const test = require('mukla')
+const isObject = require('isobject')
+const detectNextVersion = require('../src/index.js')
 
 test('return "patch" when "fix" type and scope "foo" given', (done) => {
-  const res = detectNextVersion('fix(foo): bar qux');
+  const res = detectNextVersion('fix(foo): bar qux')
 
-  test.strictEqual(res, 'patch');
-  done();
-});
+  test.strictEqual(res, 'patch')
+  done()
+})
 
 test('return "major" when "fix" type and has "BREAKING CHANGE" in body', (done) => {
   const increment = detectNextVersion(`fix(src): some very important change
-BREAKING CHANGE: we are awesome`);
+BREAKING CHANGE: we are awesome`)
 
-  test.strictEqual(increment, 'major');
-  done();
-});
+  test.strictEqual(increment, 'major')
+  done()
+})
 
 test('return "minor" when "feat" type and no scope', (done) => {
-  const inc = detectNextVersion('feat: woohoo, awesome');
+  const inc = detectNextVersion('feat: woohoo, awesome')
 
-  test.strictEqual(inc, 'minor');
-  done();
-});
+  test.strictEqual(inc, 'minor')
+  done()
+})
 
 test('return "major" when detect "BREAKING CHANGE" in header', (done) => {
-  const next = detectNextVersion('fix: BREAKING CHANGE: require node 8');
-  test.strictEqual(next, 'major');
+  const next = detectNextVersion('fix: BREAKING CHANGE: require node 8')
+  test.strictEqual(next, 'major')
 
-  const incr = detectNextVersion('fix(quxie): BREAKING CHANGE: require node 8');
-  test.strictEqual(incr, 'major');
-  done();
-});
+  const incr = detectNextVersion('fix(quxie): BREAKING CHANGE: require node 8')
+  test.strictEqual(incr, 'major')
+  done()
+})
 
 test('return "major" when type is "break" or "major"', (done) => {
-  const result1 = detectNextVersion('break(source): require node 8');
-  const result2 = detectNextVersion('major(source): require node 8');
-  const result3 = detectNextVersion('major: require node 8');
+  const result1 = detectNextVersion('break(source): require node 8')
+  const result2 = detectNextVersion('major(source): require node 8')
+  const result3 = detectNextVersion('major: require node 8')
 
-  test.strictEqual(result1, 'major');
-  test.strictEqual(result2, 'major');
-  test.strictEqual(result3, 'major');
-  done();
-});
+  test.strictEqual(result1, 'major')
+  test.strictEqual(result2, 'major')
+  test.strictEqual(result3, 'major')
+  done()
+})
 
 test('should return an "commit" object if second param is explicit true', (done) => {
-  const commit = detectNextVersion('fix(src): wooho\nmake it more generic', true);
+  const commit = detectNextVersion('fix(src): wooho\nmake it more generic', true)
 
-  test.strictEqual(isObject(commit), true);
-  test.strictEqual(commit.type, 'fix');
-  test.strictEqual(commit.scope, 'src');
-  test.strictEqual(commit.subject, 'wooho');
-  test.strictEqual(commit.header, 'fix(src): wooho');
-  test.strictEqual(commit.body, 'make it more generic');
-  test.strictEqual(commit.increment, 'patch');
-  done();
-});
+  test.strictEqual(isObject(commit), true)
+  test.strictEqual(commit.type, 'fix')
+  test.strictEqual(commit.scope, 'src')
+  test.strictEqual(commit.subject, 'wooho')
+  test.strictEqual(commit.header, 'fix(src): wooho')
+  test.strictEqual(commit.body, 'make it more generic')
+  test.strictEqual(commit.increment, 'patch')
+  done()
+})
