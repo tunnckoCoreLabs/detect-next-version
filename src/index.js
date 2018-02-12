506268f774987d6a37459638dbc786da3a0284a5
Charlike Mike Reagent <olsten.larck@gmail.com>
2018-02-12 20:04:00 +0200

    fix(deps): adjust changes of the latest parse-commit-message
    Signed-off-by: Charlike Mike Reagent <olsten.larck@gmail.com>

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@
  * @license Apache-2.0
  */
 
-const parseCommitMessage = require('parse-commit-message')
+const { parse, plugins } = require('parse-commit-message')
 
 /**
  * > Parses given `commitMessage` and returns an **"increment" type**,
@@ -46,33 +46,6 @@ const parseCommitMessage = require('parse-commit-message')
  */
 
 module.exports = function detectNextVersion (commitMessage, full) {
-  const result = parseCommitMessage(commitMessage, incrementMapper)
+  const result = parse(commitMessage, plugins)
   return full === true ? result : result.increment
-}
-
-function incrementMapper (commit) {
-  const isBreaking = isBreakingChange(commit)
-  let increment = null
-
-  if (/fix|bugfix|patch/.test(commit.type)) {
-    increment = 'patch'
-  }
-  if (/feat|feature|minor/.test(commit.type)) {
-    increment = 'minor'
-  }
-  if (/break|breaking|major/.test(commit.type) || isBreaking) {
-    increment = 'major'
-  }
-
-  return Object.assign({}, commit, { increment, isBreaking })
-}
-
-/* eslint-disable no-param-reassign */
-
-function isBreakingChange ({ subject, body, footer }) {
-  body = body || ''
-  footer = footer || ''
-
-  const re = 'BREAKING CHANGE:'
-  return subject.includes(re) || body.includes(re) || footer.includes(re)
 }
