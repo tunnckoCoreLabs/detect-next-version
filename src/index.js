0239b0d11a60b84b0e9c4c947546fc8856b6deaf
Charlike Mike Reagent <mameto2011@gmail.com>
2018-11-06 16:30:48 +0200

    fix: add docs
    Signed-off-by: Charlike Mike Reagent <mameto2011@gmail.com>

--- src/index.js
+++ src/index.js
@@ -2,6 +2,46 @@ import recommendedBump from 'recommended-bump';
 import packageJson from '@tunnckocore/package-json';
 import increment from './semver-inc';
 
+/**
+ * Calculates next version of given package with `name`,
+ * based given `commitMessages` which should follow
+ * the [Conventional Commits Specification](https://www.conventionalcommits.org/).
+ * Options are passed directly to [@tunnckocore/package-json](https://ghub.io/@tunnckocore/package-json) and
+ * [recommended-bump][] packages. Also because the recommended-bump, you can
+ * pass `options.plugins` which will be passed to [parse-commit-message][]
+ * commit message parser. So follow their docs and see the tests here for
+ * example usage. If all commit messages are of type that is not `patch|fix|minor|feat|major`
+ * or containing `BREAKING CHANGE:` label (e.g. the `chore` type), then the
+ * returned result won't have `nextVersion` and `increment` will be `false`.
+ *
+ * @example
+ * import detector from 'detect-next-version';
+ *
+ * async function main() {
+ *   const commits = ['chore(ci): some build tweaks', 'fix(cli): foo bar'];
+ *
+ *   // consider `my-npm-package` is version 0.1.0
+ *   const result = await detector('my-npm-package', commits);
+ *   console.log(result.increment); // => 'patch'
+ *   console.log(result.pkg); // => package's latest package.json metadata
+ *   console.log(result.lastVersion); // => '0.1.0'
+ *   console.log(result.nextVersion); // => '0.1.1'
+ *   console.log(result.patch[0].header.type); // => 'fix'
+ *   console.log(result.patch[0].header.scope); // => 'cli'
+ *   console.log(result.patch[0].header.subject); // => 'foobar'
+ *   console.log(result.patch[0].header.toString()); // => 'fix(cli): foobar'
+ * }
+ *
+ * main().catch(console.error);
+ *
+ * @name detectNextVersion
+ * @public
+ * @param {string} name a package name which you looking to update
+ * @param {string|string[]} commitMessages commit messages since last version
+ * @param {object} [options={}] optional, passed to above mentioned packages.
+ * @returns {object} an object which is basically the return of [recommended-bump][]
+ *                  plus `{ pkg, lastVersion, nextVersion? }`.
+ */
 export default async function detector(name, commitMessages, options = {}) {
   const opts = Object.assign({}, options);
 
@@ -28,17 +68,3 @@ export default async function detector(name, commitMessages, options = {}) {
 
   return Object.assign({}, recommended, { pkg, lastVersion, nextVersion });
 }
-
-// async function main() {
-//   const res = await detector('@hela/cli', [
-//     'chore: woohoo\n\nBREAKING CHANGE: unexpected breakage should happen',
-//     'fix: foo bar baz'
-//   ])
-
-//   console.log(res)
-//   // => res.increment === 'major' (because has "BREAKING CHANGE")
-//   // => res.lastVersion === '0.2.1'
-//   // => res.nextVersion === '1.0.0'
-// }
-
-// main()
